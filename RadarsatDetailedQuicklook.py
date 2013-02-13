a36c44ef354b06dde7e5c59c39242b961f45fc66
maxkoenig <max.koenig@npolar.no>
2013-02-13 07:55:09 +0100

    Radarsat Quicklook script v1.0

---
+++ RadarsatDetailedQuicklook.py
@@ -0,0 +1,66 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jan 16 15:43:07 2013
+
+Takes the Radarsat-2 zipfile and creates a map projected quicklook from 
+the imagery_HH file
+
+@author: max
+"""
+
+
+import zipfile, glob, os, shutil
+
+#List of zip-files to be extracted
+#filelist = glob.glob(r'C:\Users\max\Documents\trash\*.zip')
+filelist = glob.glob(r'Z:\Radarsat\Sathav\2013\02_Februar\RS2_20130201*.zip')
+
+
+#Loop through zipfiles, extract and create quicklook
+
+for radarsatfile in filelist:
+    
+    #Split names and extensions
+    (infilepath, infilename) = os.path.split(radarsatfile)
+    (infileshortname, extension) = os.path.splitext(infilename)
+    
+    #Open zipfile
+    zfile = zipfile.ZipFile(radarsatfile, 'r')
+    print    
+    print "Decompressing image for " + infilename + " on " + infilepath    
+    
+    #Extract imagery file from zipfile
+    imagefile = infileshortname + '/imagery_HH.tif'
+    #zfile.extract(imagefile, infilepath)  
+    zfile.extractall(infilepath)
+    
+    #Define names
+    #gdalsourcefile = infilepath + '\\' + infileshortname + '\\imagery_HH.tif'
+    gdalsourcefile = infilepath + '\\' + infileshortname + '\\product.xml'
+    outputfilename = infilepath + '\\' + infileshortname + '\\' + infileshortname + '_EPSG3575.tif'
+    browseimage = infilepath + '\\' + infileshortname + '_EPSG3575.tif'
+    
+    #Call gdalwarp
+    print
+    print "map projecting file"
+    print
+    os.system('gdalwarp -tps  -t_srs \"+proj=laea +lat_0=90 +lon_0=10 +x_0=0 +y_0=0 +ellps=WGS84 +datum=WGS84 +units=m +no_defs\" ' + gdalsourcefile + ' ' + outputfilename )  
+    
+    
+    #Call gdaltranslate    
+    print
+    print "downsampling file"
+    print
+    os.system('gdal_translate -ot byte -outsize 8% 8% -scale 0 50000 0 255 ' + outputfilename + ' ' + browseimage )
+
+    #Remove folder where extracted and temporary files are stored
+    shutil.rmtree(infilepath + '\\' + infileshortname )
+
+    #Close zipfile
+    zfile.close()
+    
+    
+print   
+print 'Done creating quicklooks for ', infilepath
+
+#end
---
+++ RadarsatDetailedQuicklook_EPSG3031.py
@@ -0,0 +1,66 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jan 16 15:43:07 2013
+
+Takes the Radarsat-2 zipfile and creates a map projected quicklook from 
+the imagery_HH file
+
+@author: max
+"""
+
+
+import zipfile, glob, os, shutil
+
+#List of zip-files to be extracted
+#filelist = glob.glob(r'C:\Users\max\Documents\trash\*.zip')
+#filelist = glob.glob(r'Z:\Radarsat\2012\Original\11_November\*.zip')
+filelist = glob.glob(r'Z:\Radarsat\2013\RS2_20130208*.zip') 
+
+#Loop through zipfiles, extract and create quicklook
+
+for radarsatfile in filelist:
+    
+    #Split names and extensions
+    (infilepath, infilename) = os.path.split(radarsatfile)
+    (infileshortname, extension) = os.path.splitext(infilename)
+    
+    #Open zipfile
+    zfile = zipfile.ZipFile(radarsatfile, 'r')
+    print    
+    print "Decompressing image for " + infilename + " on " + infilepath    
+    
+    #Extract imagery file from zipfile
+    imagefile = infileshortname + '/imagery_HH.tif'
+    #zfile.extract(imagefile, infilepath)  
+    zfile.extractall(infilepath)
+    
+    #Define names
+    #gdalsourcefile = infilepath + '\\' + infileshortname + '\\imagery_HH.tif'
+    gdalsourcefile = infilepath + '\\' + infileshortname + '\\product.xml'
+    outputfilename = infilepath + '\\' + infileshortname + '\\' + infileshortname + '_EPSG3031.tif'
+    browseimage = infilepath + '\\' + infileshortname + '_EPSG3031.tif'
+    
+    #Call gdalwarp
+    print
+    print "map projecting file"
+    print
+    os.system('gdalwarp -tps  -t_srs \"+proj=stere +lat_0=-90 +lat_ts=-71 +lon_0=0 +k=1 +x_0=0 +y_0=0 +ellps=WGS84 +datum=WGS84 +units=m +no_defs \" ' + gdalsourcefile + ' ' + outputfilename )  
+    
+    
+    #Call gdaltranslate    
+    print
+    print "downsampling file"
+    print
+    os.system('gdal_translate -ot byte -outsize 8% 8% -scale 0 30000 0 255 ' + outputfilename + ' ' + browseimage )
+
+    #Remove folder where extracted and temporary files are stored
+    shutil.rmtree(infilepath + '\\' + infileshortname )
+
+    #Close zipfile
+    zfile.close()
+    
+    
+print   
+print 'Done creating quicklooks for ', infilepath
+
+#end
