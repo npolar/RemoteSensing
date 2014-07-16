3c4ebad2d4b753a737490dc16d4c516ebc7e579e
maxkoenig <max.koenig@npolar.no>
2014-07-16 09:15:33 +0200

    Signed-off-by: maxkoenig <max.koenig@npolar.no>

--- AMSR/ReadAMSR.py
+++ AMSR/ReadAMSR.py
@@ -4,217 +4,16 @@ Created on Mon Jul 14 07:19:33 2014
 
 @author: max
 """
-
 import xml.etree.cElementTree as ET
-import glob, gzip
+import gdal, glob, gzip, os, shutil, sys
 
-def CreateXML(outfilepath, AMSRzipfile):
-    '''
-    Creates an XML for every AMSR file
-    http://stackoverflow.com/questions/3605680/creating-a-simple-xml-file-using-python
-    '''
-    
+def UnzipAMSR(AMSRzipfile):
+        
     #Various file paths and names:    
     (AMSRzipfilepath, AMSRzipfilename) = os.path.split(AMSRzipfile)             
     (AMSRzipfileshortname, extension) = os.path.splitext(AMSRzipfilename) 
     
-    Brigthness89 = 'HDF5:'+ AMSRzipfileshortname + '://Brightness_Temperature_(89.0GHz-A,H)'
-    Brightness_vrt = outfilepath + AMSRzipfileshortname[:-3] + '.vrt'
-    lonfile = 'HDF5:'+ AMSRzipfileshortname + '://Longitude_of_Observation_Point_for_89A'
-    latfile = 'HDF5:'+ AMSRzipfileshortname + '://Latitude_of_Observation_Point_for_89A'
-    
-    
-    root = ET.Element("VRTDataset")
-    
-    root.set("rasterXSize", "486")
-    root.set("rasterYSize", "2017")
-    
-    Metadata = ET.SubElement(root, "Metadata")
-    Metadata.set("domain", "GEOLOCATION")
-    
-    MDI =  ET.SubElement(Metadata, "MDI")
-    MDI.set("key", "X_DATASET")
-    MDI.text = outfilepath + AMSRzipfileshortname[:-3] + '_lon.vrt'
-    
-    MDI =  ET.SubElement(Metadata, "MDI")
-    MDI.set("key", "X_BAND")
-    MDI.text = '1'
-    
-    MDI =  ET.SubElement(Metadata, "MDI")
-    MDI.set("key", "Y_DATASET")
-    MDI.text = outfilepath + AMSRzipfileshortname[:-3] + '_lat.vrt'
-    
-    MDI =  ET.SubElement(Metadata, "MDI")
-    MDI.set("key", "Y_BAND")
-    MDI.text = '1'
-
-    
-    MDI =  ET.SubElement(Metadata, "MDI")
-    MDI.set("key", "PIXEL_OFFSET")
-    MDI.text = '0'
-    
-    MDI =  ET.SubElement(Metadata, "MDI")
-    MDI.set("key", "LINE_OFFSET")
-    MDI.text = '0'
-    
-    MDI =  ET.SubElement(Metadata, "MDI")
-    MDI.set("key", "PIXEL_STEP")
-    MDI.text = '1'
-    
-    MDI =  ET.SubElement(Metadata, "MDI")
-    MDI.set("key", "LINE_STEP")
-    MDI.text = '1'
-    
-    
-    VRTRasterBand = ET.SubElement(root, "VRTRasterBand" )
-    VRTRasterBand.set("datatype", "UInt16")
-    VRTRasterBand.set("band", "1")
-    
-    Metadata = ET.SubElement(root, "Metadata")
-    
-    SimpleSource = ET.SubElement(VRTRasterBand, "SimpleSource")
-    
-    SourceFilename = ET.SubElement(SimpleSource, "SourceFilename")
-    SourceFilename.set("relativeToVRT", "1")
-    
-    SourceFilename.text = Brigthness89
-    SourceBand = ET.SubElement(SimpleSource, "SourceBand")
-    SourceBand.text = "1"
-    
-    SourceProperties = ET.SubElement(SimpleSource, "SourceProperties")
-    SourceProperties.set("RasterXSize", "486")
-    SourceProperties.set("RasterYSize", "2017")
-    SourceProperties.set("DataType", "UInt16")
-    SourceProperties.set("BlockXSize", "486")
-    SourceProperties.set("BlockYSize", "1")
-    
-    SrcRect = ET.SubElement(SimpleSource, "SrcRect")
-    SrcRect.set("xOff", "0")
-    SrcRect.set("yOff", "0")
-    SrcRect.set("xSize", "486")
-    SrcRect.set("ySize", "2017")
-    
-    DstRect = ET.SubElement(SimpleSource, "DstRect")
-    DstRect.set("xOff", "0")
-    DstRect.set("yOff", "0")
-    DstRect.set("xSize", "486")
-    DstRect.set("ySize", "2017")
-    
-    tree = ET.ElementTree(root)
-    tree.write(Brightness_vrt)
-
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    ###Create lon.vrt
-    
-    root = ET.Element("VRTDataset")
-    
-    root.set("rasterXSize", "486")
-    root.set("rasterYSize", "2017")
-    
-    VRTRasterBand = ET.SubElement(root, "VRTRasterBand" )
-    VRTRasterBand.set("datatype", "Float32")
-    VRTRasterBand.set("band", "1")
-    
-    Metadata = ET.SubElement(VRTRasterBand, "Metadata")
-    SimpleSource = ET.SubElement(VRTRasterBand, "SimpleSource")
-    
-    SourceFilename = ET.SubElement(SimpleSource, "SourceFilename")
-    SourceFilename.set("relativeToVRT", "1")
-    SourceFilename.text = lonfile
-    SourceBand = ET.SubElement(SimpleSource, "SourceBand")
-    SourceBand.text = "1"
-    
-    SourceProperties = ET.SubElement(SimpleSource, "SourceProperties")
-    SourceProperties.set("RasterXSize", "486")
-    SourceProperties.set("RasterYSize", "2017")
-    SourceProperties.set("DataType", "Float32")
-    SourceProperties.set("BlockXSize", "486")
-    SourceProperties.set("BlockYSize", "1")
-    
-    SrcRect = ET.SubElement(SimpleSource, "SrcRect")
-    SrcRect.set("xOff", "0")
-    SrcRect.set("yOff", "0")
-    SrcRect.set("xSize", "486")
-    SrcRect.set("ySize", "2017")
-    
-    DstRect = ET.SubElement(SimpleSource, "DstRect")
-    DstRect.set("xOff", "0")
-    DstRect.set("yOff", "0")
-    DstRect.set("xSize", "486")
-    DstRect.set("ySize", "2017")
-    
-    tree = ET.ElementTree(root)
-    tree.write(outfilepath + AMSRzipfileshortname[:-3] + '_lon.vrt')
-    
-    
-    
-    
-    
-    
-    root = ET.Element("VRTDataset")
-    
-    root.set("rasterXSize", "486")
-    root.set("rasterYSize", "2017")
-    
-    VRTRasterBand = ET.SubElement(root, "VRTRasterBand" )
-    VRTRasterBand.set("datatype", "Float32")
-    VRTRasterBand.set("band", "1")
-    
-    Metadata = ET.SubElement(VRTRasterBand, "Metadata")
-    SimpleSource = ET.SubElement(VRTRasterBand, "SimpleSource")
-    
-    SourceFilename = ET.SubElement(SimpleSource, "SourceFilename")
-    SourceFilename.set("relativeToVRT", "1")
-    SourceFilename.text = latfile
-    SourceBand = ET.SubElement(SimpleSource, "SourceBand")
-    SourceBand.text = "1"
-    
-    SourceProperties = ET.SubElement(SimpleSource, "SourceProperties")
-    SourceProperties.set("RasterXSize", "486")
-    SourceProperties.set("RasterYSize", "2017")
-    SourceProperties.set("DataType", "Float32")
-    SourceProperties.set("BlockXSize", "486")
-    SourceProperties.set("BlockYSize", "1")
-    
-    SrcRect = ET.SubElement(SimpleSource, "SrcRect")
-    SrcRect.set("xOff", "0")
-    SrcRect.set("yOff", "0")
-    SrcRect.set("xSize", "486")
-    SrcRect.set("ySize", "2017")
-    
-    DstRect = ET.SubElement(SimpleSource, "DstRect")
-    DstRect.set("xOff", "0")
-    DstRect.set("yOff", "0")
-    DstRect.set("xSize", "486")
-    DstRect.set("ySize", "2017")
-    
-    tree = ET.ElementTree(root)
-    tree.write(outfilepath + AMSRzipfileshortname[:-3] + '_lat.vrt')
-    
-    print "created XML"
-    print outfilepath + AMSRzipfileshortname[:-3] + '_lat.vrt'
-    print Brightness_vrt
-    ###Create lon.vrt
-    
-def ConvertAMSR(outfilepath, AMSRzipfile):
-
-    #Various file paths and names:    
-    (AMSRzipfilepath, AMSRzipfilename) = os.path.split(AMSRzipfile)             
-    (AMSRzipfileshortname, extension) = os.path.splitext(AMSRzipfilename) 
-    
-    print AMSRzipfile
+    print 'extracting ', AMSRzipfile
     AMSRzipfile = gzip.open(AMSRzipfile)
     AMSRopened = AMSRzipfile.read()
     AMSRzipfile.close()
@@ -222,32 +21,113 @@ def ConvertAMSR(outfilepath, AMSRzipfile):
     AMSRfile = file((outfilepath + AMSRzipfileshortname), 'wb')
     AMSRfile.write(AMSRopened)
     AMSRfile.close()
-    
-    
-    AMSR_vrt = outfilepath + AMSRzipfileshortname[:-3] + '.vrt'
-    AMSR_temp_tif = outfilepath + 'temp.tif'
-    AMSR_temp2_tif = outfilepath + 'temp2.tif'
-    AMSR_tif = outfilepath + AMSRzipfileshortname[:-3] + '.tif'
-    
-    print AMSR_vrt
-    
-    print "gdalwarp"
-    os.system('gdalwarp -geoloc -t_srs EPSG:4326 ' + AMSR_vrt + ' ' +  AMSR_temp_tif)
-    print "gdal_translate"
-    os.system('gdal_translate -projwin -94 90 40 35 ' + AMSR_temp_tif + ' ' +   AMSR_temp2_tif)  
-    os.system('gdalwarp -s_srs EPSG:4326 -t_srs EPSG:3411 ' + AMSR_temp2_tif + ' ' + AMSR_tif)
-    
-    #os.remove(AMSR_temp_tif)
-    #os.remove(AMSR_temp2_tif)
-    
-    AMSRfile.close()
-    
-    
-    
-    
 
 
 
+def AMSRtoCSV(AMSRzipfile):
+    '''
+    converts AMSR raster to csv
+    '''
+    (AMSRzipfilepath, AMSRzipfilename) = os.path.split(AMSRzipfile)             
+    (AMSRzipfileshortname, extension) = os.path.splitext(AMSRzipfilename)
+    
+    AMSRcsv = outfilepath + AMSRzipfileshortname[:-3] + '.csv'
+    
+    HDFfile = gdal.Open(AMSRzipfilepath + '\\' + AMSRzipfileshortname )
+    HDF_bands = HDFfile.GetSubDatasets()
+    
+    HDF_Br89AH = gdal.Open(HDF_bands[13][0])
+    HDF_Br89AH_array = HDF_Br89AH.ReadAsArray()
+    
+    HDF_Br89BH = gdal.Open(HDF_bands[15][0])
+    HDF_Br89BH_array = HDF_Br89BH.ReadAsArray()
+    
+    HDF_Lat89A = gdal.Open(HDF_bands[27][0])
+    HDF_Lat89A_array = HDF_Lat89A.ReadAsArray()
+    
+    HDF_Lat89B = gdal.Open(HDF_bands[28][0])
+    HDF_Lat89B_array = HDF_Lat89B.ReadAsArray()
+    
+    HDF_Lon89A = gdal.Open(HDF_bands[29][0])
+    HDF_Lon89A_array = HDF_Lon89A.ReadAsArray()
+    
+    HDF_Lon89B = gdal.Open(HDF_bands[30][0])
+    HDF_Lon89B_array = HDF_Lon89B.ReadAsArray()
+    
+    
+    cols = HDF_Br89AH.RasterXSize
+    rows = HDF_Br89AH.RasterYSize
+    
+    print 'Convert  ', AMSRzipfileshortname, ' to csv.'    
+    
+    textfile = open( AMSRcsv, 'w')
+    textfile.write('lon, lat, brightness\n')
+    for i in range(rows):
+        for j in range(cols):
+            lonA = HDF_Lon89A_array[i,j]
+            latA = HDF_Lat89A_array[i,j]
+            brightnessA = HDF_Br89AH_array[i,j]
+            
+            lonB = HDF_Lon89B_array[i,j]
+            latB = HDF_Lat89B_array[i,j]
+            brightnessB = HDF_Br89BH_array[i,j]
+            
+            if 40 < latA < 90:
+                textfile.write(str(lonA) + ', ' + str(latA) + ', ' + str(brightnessA) + '\n')
+
+            if 40 < latB < 90:
+                textfile.write(str(lonB) + ', ' + str(latB) + ', ' + str(brightnessB) + '\n')
+            
+    textfile.close()
+    
+    
+    
+def CSVtoRaster(AMSRzipfile):
+    '''
+    converts CSV raster to raster
+    '''
+    
+    (AMSRzipfilepath, AMSRzipfilename) = os.path.split(AMSRzipfile)             
+    (AMSRzipfileshortname, extension) = os.path.splitext(AMSRzipfilename)
+         
+    AMSRcsv_shortname = AMSRzipfileshortname[:-3]
+    AMSRcsv = outfilepath + AMSRzipfileshortname[:-3] + '.csv'
+    
+    AMSRcsv_vrt = outfilepath + AMSRzipfileshortname[:-3] + '.vrt'
+    AMSRcsv_tif = outfilepath + AMSRzipfileshortname[:-3] + '.tif'
+    ##############################
+    #Create the necessary XML file
+    ##############################
+    root = ET.Element("OGRVRTDataSource")
+    OGRVRTLayer  = ET.SubElement(root, "OGRVRTLayer")
+    OGRVRTLayer.set("name", AMSRcsv_shortname)
+    
+    SrcDataSource = ET.SubElement(OGRVRTLayer, "SrcDataSource")
+    SrcDataSource.text = AMSRcsv
+    
+    GeometryType = ET.SubElement(OGRVRTLayer, "GeometryType")
+    GeometryType.text = "wkbPoint"
+    
+    GeometryField = ET.SubElement(OGRVRTLayer,"GeometryField")
+    GeometryField.set("encoding", "PointFromColumns")
+    GeometryField.set("x", "lon")
+    GeometryField.set("y", "lat")
+    GeometryField.set("z", "brightness")
+    
+    tree = ET.ElementTree(root)
+    tree.write(AMSRcsv_vrt)
+    
+    ##########################
+    # Gdal_grid
+    ##########################
+    
+    print "Convert csv to raster."
+    os.system('gdal_grid -a average -l ' + AMSRcsv_shortname + ' '  + AMSRcsv_vrt + ' ' + AMSRcsv_tif)
+
+    
+    
+    
+    
 
 
     
@@ -258,13 +138,16 @@ def ConvertAMSR(outfilepath, AMSRzipfile):
 ##############################################################################
 
 outfilepath = 'C:\\Users\\max\\Documents\\AMSR\\'
-filelist = glob.glob(r'C:\Users\max\Documents\AMSR\*.gz')
+filelist = glob.glob(r'C:\Users\max\Documents\AMSR\GW1AM2_201301010158_201D_L1SGBTBR_1110110*.gz')
 
 for AMSRzipfile in filelist:
     
+    UnzipAMSR(AMSRzipfile)
+    AMSRtoCSV(AMSRzipfile)
+    CSVtoRaster(AMSRzipfile)
     
-    CreateXML(outfilepath, AMSRzipfile)
-    ConvertAMSR(outfilepath, AMSRzipfile)
+    
+print 'Done ReadAMSR.py'
 
       
     
